1f3c5ae2a52fb9604640917d65a4b3155c54fb8d
incoCodes <supersonic220019@gmail.com>
2022-03-13 07:01:07 +0300

    Added a death animation to the animator

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,19 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
+       public Animator animator;
        [SerializeField] float health = 100f;
 
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health -= damage, 0);
             Debug.Log(health);
+
+            if (health == 0)
+            {
+                animator.SetTrigger("dead");
+            }
         }
+
     }
 }
